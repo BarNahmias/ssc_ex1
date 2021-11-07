5f14dec0d04a235b9d17296c4d58ba0ea65eb67d
BarNahmias <92825016+BarNahmias@users.noreply.github.com>
2021-11-07 23:52:49 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
 #include <math.h>
 #include "NumClass.h"
-/*#include  "advancedClassificationLoop.h"
-#include "advancedClassificationRecursion.h" 
-#include "basicClassification.h" 
-#include "libclassloops.a"
-#include "libclassloops.so"
-#include "libclassrec.so"
-#include "libclassrec.a"
-*/
 
 
 int main(){
@@ -17,28 +9,28 @@ int main(){
     scanf("%d",&a);
     scanf("%d",&b);
 
-    printf("The Armstrong numbers are: ");
+    printf("The Armstrong numbers are:");
     for(int i=a; i<=b;i++){
         if(isArmstrong(i)==1){
            printf("%d",i);
             printf(" ");
         }
     }
-    printf("The Palindromes are: ");
+    printf("The Palindromes are:");
     for(int i=a; i<=b;i++){
         if(isPalindrome(i)==1){
            printf("%d",i);
             printf(" ");
         }
     }
-    printf("The Prime numbers are: ");
+    printf("The Prime numbers are:");
     for(int i=a; i<=b;i++){
         if(isPrime(i)==1){
            printf("%d",i);
             printf(" ");
         }
     }
-    printf("The Strong numbers are: ");
+    printf("The Strong numbers are:");
     for(int i=a; i<=b;i++){
         if(isStrong(i)==1){
            printf("%d",i );
