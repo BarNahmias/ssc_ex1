138db2adfd8835c85c3127b7ba1272876e7438b6
BarNahmias <92825016+BarNahmias@users.noreply.github.com>
2021-11-08 23:48:16 +0200

    Add files via upload

--- NumClass.h
+++ NumClass.h
@@ -1,4 +1,9 @@
-int isArmstrong(int) ;
-int isPalindrome(int);
-int isPrime(int);
-int isStrong(int);
+#ifndef Numclass_H_
+#define Numclass_H_
+
+    int isArmstrong(int) ;
+    int isPalindrome(int);
+    int isPrime(int);
+    int isStrong(int);
+
+#endif
